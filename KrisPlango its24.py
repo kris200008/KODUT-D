1698c508bfa1447679f5d650b67b2f8b38d34a84
kris200008 <krispalango@hkhk.edu.ee>
2025-02-02 08:18:55 -0800

    Add files via upload

---
+++ KrisPlango its24.py
@@ -0,0 +1,13 @@
+import turtle
+
+for i in range (4):
+    turtle.forward(150)
+    turtle.right(-90)
+    turtle.forward(50)
+    turtle.right(-90)
+    turtle.forward(150)
+    turtle.right(-90)
+    turtle.forward(50)
+
+
+turtle.done()
